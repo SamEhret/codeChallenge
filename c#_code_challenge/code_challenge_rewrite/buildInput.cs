85199bbd747ef03a5b48279d60983546aa795968
Sam Ehret <ehret.sam@yahoo.com>
2018-05-09 16:27:45 -0500

    added input processing functions

--- c#_code_challenge/code_challenge_rewrite/buildInput.cs
+++ c#_code_challenge/code_challenge_rewrite/buildInput.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace code_challenge_rewrite
 {
@@ -14,7 +17,45 @@ namespace code_challenge_rewrite
             {
                 inputString = "(id,created,employee(id,firstname,employeeType(id),lastname),location)";
             }
-            Console.WriteLine(inputString);
+
+            isValid(inputString);
+        }
+
+        public void isValid(string inputString)
+        {
+            if (inputString.Count(a => a == '(') != inputString.Count(b => b == ')'))
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+            else if (inputString[0] != '(' || inputString[(inputString.Length - 1)] != ')')
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+            else
+            {
+                processInput(inputString);
+            }
+        }
+
+        public void processInput(string inputString)
+        {
+            Regex search = new Regex(@"\((.+)\)");
+            inputString = search.Match(inputString).Groups[1].Value;
+
+            string[] splitResults = Regex.Split(inputString, @"([(),])");
+
+            List<string> newList = new List<string>();
+
+            foreach (var s in splitResults)
+            {
+                if (s != "" && s != ",")
+                {
+                    newList.Add(s);
+                }
+            }
+            //Next pass the list over to the tree builder and set up the node class
         }
     }
 }
